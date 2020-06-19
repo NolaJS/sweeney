fca81a1a9019e2a8f6228a595776cdbffe99ed05
Steven Scaffidi <Steven.Scaffidi@bottomline.com>
2020-06-18 19:43:05 -0500

    sweeney updates

--- layout/Footer.js
+++ layout/Footer.js
@@ -1,24 +1,35 @@
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Container } from 'reactstrap';
 
 const locale = {
+  address: '4333 Washington Ave.',
+  address2: 'New Orleans, LA 70125',
   by: 'NolaJS',
   company: 'Sweeney Restoration',
   copyright: 'Copyright',
   design: 'Designed and Built by',
-  phone: '555-555-5555',
+  email: 'info@sweeneyrestoration.com',
+  phone: '504-533-0007',
 };
 
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <Row className="fixed-bottom bg-dark p-3 text-white">
-      <Col>
-        {locale.copyright} {year} {locale.company}
-      </Col>
-      <Col className="text-right">
-        {locale.design} {locale.by}
-      </Col>
-    </Row>
+    <Container fluid className="bg-dark p-3 text-white">
+      <Row>
+        <Col>
+          {locale.company}
+          4333 Washington Ave.
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          {locale.copyright} {year} {locale.company}
+        </Col>
+        <Col className="text-right">
+          {locale.design} {locale.by}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
